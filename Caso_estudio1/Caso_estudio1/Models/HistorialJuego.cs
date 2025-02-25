d3bae7b51fcc04044586904a9df94ef86ce18044
Maria Celeste Solano Hidalgo <ms2307628@gmail.com>
2025-02-24 20:02:07 -0600

    modelo del historial

---
+++ Caso_estudio1/Caso_estudio1/Models/HistorialJuego.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class HistorialJuego
+{
+    public int Id { get; set; } // Identificador único del historial de la partida
+
+    public int UserId { get; set; } // Referencia al usuario que jugó
+
+    public int Score { get; set; } // Puntuación del jugador
+
+    public TimeSpan Duration { get; set; } // Duración de la partida
+
+    public DateTime DatePlayed { get; set; } // Fecha y hora de la partida
+
+    // Navegación a la entidad User
+    public virtual User User { get; set; }
+}
